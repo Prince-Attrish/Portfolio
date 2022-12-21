534c31ae30140fa6ec885b5affa2331afe28250a
Prince-Attrish <vs3031999@gmail.com>
2022-12-21 22:20:07 +0530

    Change title when user leaves using js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var nav = document.querySelector('nav');
-
 window.addEventListener('scroll', function(){
     if (window.pageYOffset > 50) {
         nav.classList.add('bg-dark', 'shadow')
     } else {
         nav.classList.remove('bg-dark', 'shadow')
     }
+})
+
+// ------------- Change title when user leaves ----------->
+
+var docTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Prince ~ See ya!";
+})
+window.addEventListener("focus", () => {
+    document.title = docTitle;
 })
